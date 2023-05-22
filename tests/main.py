2f8f2c5eaad12e491375b113474f4495db9c2b7a
novis10813 <zhizhongz895253@gmail.com>
2023-05-22 12:10:34 +0800

    update file structure
    - Update file structure to make it more like a package

---
+++ tests/main.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+
+from agent import DQNAgent
+
+# from wrappers import env_wrapper
+# env = env_wrapper(env)
+# |%%--%%| <SdxHUdFXGS|ioCUm2CuEK>
+
+
+# env = gym.make("ALE/Asteroids-v5", render_mode="human", obs_type="ram")
+env = gym.make("CartPole-v1", render_mode="human")
+env.metadata["render_fps"] = 15
+agent = DQNAgent(env=env, verbose="debug")
+agent.train()
